26399f753ab1fa549405c8169d907b239a1dbc5a
Yangseongwoo <163240206+Yangseongwoo@users.noreply.github.com>
2024-11-18 00:21:08 +0900

    11.18

--- js/mobilenav.js
+++ js/mobilenav.js
@@ -2,7 +2,7 @@ document.addEventListener('DOMContentLoaded', function() {
     const menuBtn = document.querySelector('.menu-btn');
     const slideMenu = document.querySelector('.slide-menu');
     
-    menuBtn.addEventListener('click', function() {
+    menuBtn.addEventListener('touchstart', function() {
         this.classList.toggle('active');
         slideMenu.classList.toggle('active');
     });
